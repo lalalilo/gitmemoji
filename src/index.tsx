1e5a30fc2df75692458d14b9a8f4a064e00019df
max-programming <maxprogramming05@gmail.com>
2020-12-18 11:48:05 +0530

    ✨ Added new working serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
